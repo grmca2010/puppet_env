91db421a31c75368de94d06d51d5a45ae62bd1b4
sgundepa <sgundepa@cisco.com>
2015-09-15 15:30:31 +0530

    added script sgundepa-aem

--- branch.pp
+++ branch.pp
@@ -1,3 +1,12 @@
 Exec { path => [ "/usr/bin", "/bin", "/usr/sbin", "/usr/local/node/node-default/bin" ] }
-$DEVOPS_BRANCH="senthil"
+
+if $hostname =~ /^(adamantium|artichoke|horseradish|onion|parsley|parsnip|pattypan|puppetdev|unobtainium|wiheller-puppet|wiheller-cec)(\.cisco\.com)?$/ {
+    $gitflowbranch = 'develop'
+  } elsif $hostname =~ /^(sgundepa-aem)(\.cisco\.com)?$/ {
+    $gitflowbranch = 'feature/11531'
+  } else {
+    $gitflowbranch = 'master'
+  }
+$DEVOPS_BRANCH=  $gitflowbranch
+
 include my_module
